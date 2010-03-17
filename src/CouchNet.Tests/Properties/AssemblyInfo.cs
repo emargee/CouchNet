841a5984d956caa3c19010b2390ccf98932a0590
[mRg] <emargee@googlemail.com>
2010-03-17 15:58:21 +0000

    Basic project structure and more lib files

---
+++ src/CouchNet.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CouchNet UnitTests")]
+[assembly: AssemblyDescription("CouchDB Library Unit Tests")]
+[assembly: AssemblyCompany("The CouchNet Project")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if Debug
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7A042FD1-A4AB-4ED7-B61F-3BC80FA6FC10")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ src/CouchNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CouchNet")]
+[assembly: AssemblyDescription("CouchDB Library")]
+[assembly: AssemblyCompany("The CouchNet Project")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if Debug
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E2FBF3A2-0346-4CBC-8CF6-5957C8CAFDF2")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: InternalsVisibleTo("CouchNet.Tests")]
